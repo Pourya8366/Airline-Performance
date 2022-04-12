dc1621c4138e9d0031407c7f30cffe720cdb75ad
Pourya Valizadeh <pvalizadeh1374@gmail.com>
2022-04-12 18:50:20 +0200

    graph initialized

--- query.sql
+++ query.sql
@@ -4,6 +4,9 @@ union
 select Dest 
 from flights
 
-select Origin, Dest, Reporting_Airline, sum(cast(DepDelay as int)) as Departure_Delay, sum(cast(ArrDelay as int)) as Arrival_Delay, sum(cast(ActualElapsedTime as int) - cast(CRSElapsedTime as int)) as Wasted_Time, sum(cast(Cancelled as int)) as Cancelled, sum(cast(Diverted as int)) as Diverted, count(*) as Flights_Count 
+select Origin as src, Dest as dst, Reporting_Airline as relationship, sum(cast(DepDelay as int)) as Departure_Delay, sum(cast(ArrDelay as int)) as Arrival_Delay, sum(cast(ActualElapsedTime as int) - cast(CRSElapsedTime as int)) as Wasted_Time, sum(cast(Cancelled as int)) as Cancelled, sum(cast(Diverted as int)) as Diverted, count(*) as Flights_Count 
 from flights 
 group by Origin, Dest, Reporting_Airline
+
+
+select Origin as src, Dest as dst, Reporting_Airline as airline, Year, Quarter, Month, DayofMonth, DayOfWeek, Origin, OriginStateName, Dest, DestStateName, DepDelay, ArrDelay, CRSDepTime, DepTime, Cancelled, CRSElapsedTime, ActualElapsedTime, Distance from flights 
