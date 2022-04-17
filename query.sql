379ff5651fab88071632586288d6d6d32721e306
Pourya Valizadeh <pvalizadeh1374@gmail.com>
2022-04-17 22:50:24 +0200

    add some queries and corellation

--- query.sql
+++ query.sql
@@ -1,12 +1,49 @@
+-- select unique airports as vertices
 select Origin as airport 
 from flights 
 union 
 select Dest 
 from flights
 
-select Origin as src, Dest as dst, Reporting_Airline as relationship, sum(cast(DepDelay as int)) as Departure_Delay, sum(cast(ArrDelay as int)) as Arrival_Delay, sum(cast(ActualElapsedTime as int) - cast(CRSElapsedTime as int)) as Wasted_Time, sum(cast(Cancelled as int)) as Cancelled, sum(cast(Diverted as int)) as Diverted, count(*) as Flights_Count 
-from flights 
-group by Origin, Dest, Reporting_Airline
+-- select fields from flights as edges
+select Origin as src, Dest as dst, Reporting_Airline as airline, Year, Quarter, Month, DayofMonth, DayOfWeek, Origin, OriginStateName, OriginCityName, Dest, DestStateName, DestCityName, cast(DepDelay as int) as Departure_delay, cast(ArrDelay as int) as Arrival_Delay, DepTimeBlk, cast(Cancelled as int) as Cancelled, cast(CRSElapsedTime as int) as CRSElapsed_Time, cast(ActualElapsedTime as int) as Actual_Elapsed_Time, Distance from flights
 
+---- Time ---- 
 
-select Origin as src, Dest as dst, Reporting_Airline as airline, Year, Quarter, Month, DayofMonth, DayOfWeek, Origin, OriginStateName, Dest, DestStateName, DepDelay, ArrDelay, CRSDepTime, DepTime, Cancelled, CRSElapsedTime, ActualElapsedTime, Distance from flights 
+-- year-based arrival delay
+select Year, avg(ArrDelay) as Arrival_Delay, sum(Cancelled) as Total_Cancelled from stats group by Year order by Arrival_Delay desc, Total_Cancelled desc
+
+-- month-based arrival delay
+select Year, Month, avg(ArrDelay) as Arrival_Delay, sum(Cancelled) as Total_Cancelled from stats group by Year, Month order by Arrival_Delay desc, Total_Cancelled desc
+
+-- day-based arrival delay
+select Year, Month, DayofMonth as Day, avg(ArrDelay) as Arrival_Delay, sum(Cancelled) as Total_Cancelled from stats group by Year, Month, Day order by Arrival_Delay desc, Total_Cancelled desc
+
+-- dayOfWeek-based arrival delay
+select Year, Month, DayOfWeek, avg(ArrDelay) as Arrival_Delay, sum(Cancelled) as Total_Cancelled from stats group by Year, Month, DayOfWeek order by Arrival_Delay desc, Total_Cancelled desc
+
+-- hourly delay
+select DepTimeBlk, avg(ArrDelay) as Arrival_Delay from stats group by DepTimeBlk order by Arrival_Delay desc
+
+---- Airline ----
+
+-- ailrine ranking worst to best
+select airline, avg(ArrDelay) as Arrival_Delay, sum(Cancelled) as Total_Cancelled from stats group by airline order by Arrival_Delay desc, Total_Cancelled desc
+
+-- airline yearly based
+airline_yearly = select Year, airline, avg(ArrDelay) as Arrival_Delay, sum(Cancelled) as Total_Cancelled from stats group by Year, airline 
+
+-- worst airline of each year
+select Year, airline, max(Arrival_Delay) as Arrival_Delay from airline_yearly group by Year
+
+-- best airline of each year
+select Year, airline, min(Arrival_Delay) as Arrival_Delay from airline_yearly group by Year
+
+-- airline performance
+select Year, Month, airline, avg(ArrDelay) as Arrival_Delay, sum(Cancelled) as Total_Cancelled from stats group by Year, Month order by Arrival_Delay desc, Total_Cancelled desc
+
+-- longest distance
+select src, dst, max(Distance) as Distance from stats group by src, dst order by Distance
+
+---- airports ----
+select src, dst, avg(Arrival_Delay) as Arrival_Delay from stats group by src, dst order by Arrival_Delay desc
